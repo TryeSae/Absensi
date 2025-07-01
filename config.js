c93f58325aa2d50eab8676b298693537e08cfcb2
TryeSae <tryesae263@gmail.com>
2025-07-01 18:04:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "https://feriqnmbfzixgeedmvzw.supabase.co",
+  "https://sukqpqlmuhcpakyadmvy.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1a3FwcWxtdWhjcGFreWFkbXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzNjU5MzIsImV4cCI6MjA2Njk0MTkzMn0.MrzubAP7PvU8oySrq1suXuImirAqyd-KxDFsi_D_vFk" 
 );
