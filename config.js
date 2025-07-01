d81bfcc123910c396dd010fc1867d12714ab8a21
TryeSae <tryesae263@gmail.com>
2025-07-01 18:01:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-const SUPABASE_URL = 'https://sukqpqlmuhcpakyadmvy.supabase.co'; 
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1a3FwcWxtdWhjcGFreWFkbXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzNjU5MzIsImV4cCI6MjA2Njk0MTkzMn0.MrzubAP7PvU8oySrq1suXuImirAqyd-KxDFsi_D_vFk'; 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-const { createClient } = supabase;
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-window.supabase = supabaseClient;
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://feriqnmbfzixgeedmvzw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1a3FwcWxtdWhjcGFreWFkbXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzNjU5MzIsImV4cCI6MjA2Njk0MTkzMn0.MrzubAP7PvU8oySrq1suXuImirAqyd-KxDFsi_D_vFk" 
+);
